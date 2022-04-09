cf0517867c0e0b0d4fd50af803ee8d9737198109
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-09 23:15:20 +0530

    Update db.ts

--- middlewares/db.ts
+++ middlewares/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import {errorHandler} from './error';
 
 const connectDB = (handler) => errorHandler(async (req, res) => {
-  console.log(req.connection.remoteAddress);
+  console.log(req.connection.remoteAddress, req.headers["x-real-ip"]);
   if (req.headers.host !== process.env.HOST) {
     res.status(400);
     throw new Error('not allowed');
