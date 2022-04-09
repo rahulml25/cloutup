9d459fc08e75e36424f9b72ee64a98c48fe3d15c
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-09 21:34:19 +0530

    Update db.ts

--- middlewares/db.ts
+++ middlewares/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import {errorHandler} from './error';
 
 const connectDB = (handler) => errorHandler(async (req, res) => {
+  console.log(req.headers.host);
   if (!mongoose.connections[0].readyState) {
     await mongoose.connect(process.env.DATABASE_URI);
   }
