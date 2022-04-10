fe93d6519c05ff364c27f7bd66542fca1bbfb6cf
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-10 14:47:35 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Sidebar from 'components/Sidebar';
+import Sidebar from 'components/sidebar';
 import Feed from 'components/Feed';
 import Views from 'components/Views';
 
