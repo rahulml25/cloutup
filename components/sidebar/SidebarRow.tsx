75a561f1471941e7ca4cce94ea953e467e1d1f3c
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-10 21:46:28 +0530

    Update SidebarRow.tsx

--- components/sidebar/SidebarRow.tsx
+++ components/sidebar/SidebarRow.tsx
@@ -1,7 +1,7 @@
 
 const SidebarRow = ({Icon, title}) => {
   return (
-  	<div className="flex md:p-1.5 items-center hover:bg-gray-200 rounded-md cursor-pointer">
+  	<div className="flex md:p-1.5 items-center sm:justify-center lg:justify-left hover:bg-gray-200 rounded-md cursor-pointer">
       <Icon className="h-5 md:h-6 text-blue-500"/>
       <p className="hidden lg:inline ml-2">{title}</p>
   	</div>
