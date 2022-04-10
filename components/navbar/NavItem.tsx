37eef8039a0d477aea95a20519ac8ed2feb04962
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-10 19:54:53 +0530

    Update NavItem.tsx

--- components/navbar/NavItem.tsx
+++ components/navbar/NavItem.tsx
@@ -3,7 +3,7 @@
 const NavItem = ({Icon, }) => {
   return (
   	<div className="md:p-4 md:bg-gray-100 rounded-xl cursor-pointer">
-  	  <Icon className="h-6 md:h-7 text-gray-500 hover:text-blue-500"/>
+  	  <Icon className="h-5 md:h-6 text-gray-500 hover:text-blue-500"/>
   	</div>
   )
 }
