ef5ee8753bb782ef45c45c5882b6d11b86b0b4d3
rahulml2505 <rahulml2505@gmail.com>
2022-04-10 05:40:18 +0000

    navitem testing

---
+++ components/navbar/NavIcon.tsx
@@ -0,0 +1,6 @@
+
+const NavIcon = () => {
+
+}
+
+export default  NavIcon;
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
         {/* Center Nav */}  
         <div className="flex items-center space-x-3 cursor-pointer">
-          <NavItem icon={HomeIcon}/>
+          <NavItem Icon={HomeIcon}/>
           <div className="md:p-4 md:bg-gray-100 rounded-xl">
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
               width="24" height="24"
