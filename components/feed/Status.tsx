2d904cdc086070c2db591909551617f12fd018c7
Rahul <80871183+rahulml2505@users.noreply.github.com>
2022-04-10 19:38:45 +0530

    Update Status.tsx

--- components/feed/Status.tsx
+++ components/feed/Status.tsx
@@ -1,18 +1,16 @@
 
-const Status = (props) => {
-  const {userSrc, statusSrc} = props;
-
+const Status = ({userSrc, statusSrc, children}) => {
   return (
     <div className="relative cursor-pointer">
       <img
   	    src={userSrc}
   	    className="absolute m-1 p-0.5 h-10 bg-white rounded-full shadow z-[3]" alt=""
       />
+      {children}
   	  <img
   	    src={statusSrc}
   	    className="h-32 rounded-xl hover:scale-[1.05] shadow-md z-[1]" alt=""
   	  />
-  	  {props?.children}
   	</div>
   )
 }
